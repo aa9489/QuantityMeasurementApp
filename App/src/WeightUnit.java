44c5eed1abd9ed1aad21192fc447fa94e371745a
aa9489 <aa9489@srmist.edu.in>
2026-05-07 23:29:13 +0530

    aa9489 added uc10

--- App/src/WeightUnit.java
+++ App/src/WeightUnit.java
@@ -1,4 +1,4 @@
-public enum WeightUnit {
+public enum WeightUnit implements IMeasurable {
 
     KILOGRAM(1.0),
     GRAM(0.001),
@@ -10,17 +10,23 @@ public enum WeightUnit {
         this.conversionFactor = conversionFactor;
     }
 
+    @Override
     public double getConversionFactor() {
         return conversionFactor;
     }
 
-    // Convert current unit to base unit (Kilogram)
+    @Override
     public double convertToBaseUnit(double value) {
         return value * conversionFactor;
     }
 
-    // Convert base unit (Kilogram) to current unit
+    @Override
     public double convertFromBaseUnit(double baseValue) {
         return baseValue / conversionFactor;
+    }
+
+    @Override
+    public String getUnitName() {
+        return this.name();
     }
 }
